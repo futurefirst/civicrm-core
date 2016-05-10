8e9df1950c8aa9cecd5599f172dd393ac1b7cbd3
yashodha <yashodha.chaku@webaccessglobal.com>
2016-05-10 16:48:05 +0530

    CRM-18521: Search results table, 'Select all' checkbox gives 'mark_x_201 is not of the type Integer'
    ----------------------------------------
    * CRM-18521: Search results table, "Select all" checkbox gives  "mark_x_201 is not of the type Integer"
      https://issues.civicrm.org/jira/browse/CRM-18521

--- CRM/Contact/Page/AJAX.php
+++ CRM/Contact/Page/AJAX.php
@@ -1207,7 +1207,7 @@ LIMIT {$offset}, {$rowCount}
         foreach ($elements as $key => $element) {
           $elements[$key] = self::_convertToId($element);
         }
-	CRM_Utils_Type::escapeAll($elements, 'Integer');
+        CRM_Utils_Type::escapeAll($elements, 'Integer');
         CRM_Core_BAO_PrevNextCache::markSelection($cacheKey, $actionToPerform, $elements);
       }
       else {
